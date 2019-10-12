bf950a8708afa110ba389e9fa1b3ff2085e63464
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2019-10-12 12:21:02 -0700

    Toy problem1 and some early experiment observations - refer to ReadMe

---
+++ ToyProblems/ReadMe.md
@@ -0,0 +1,17 @@
+I want to experiment with toy problems that are simpler than reward design for 3-sisters. This folder is dedicated to such problems.
+
+# Problem1
+Consider single  cell and solve temporal constraints between corn and bean
+
+## formulation
+- Environment: single cell with state as 2 x 1 vector, first value representing corn and second value bean.
+- ActionSpace: When first value is 0, we have 2 possible actions plant corn or leave empty. Similarly for second value for bean. Note that action space is a function of state space (i.e., available actions change with current state).
+- Reward: Bean when planted after a few days of corn will have higher reward. Corn with bean will have higher reward.
+- Question: Can an agent learn the above temporal constraint?
+  - Start with simple Q-Learning Tabular agent (because the action space and state space is small)
+
+## Observations / Issues
+- As of now, here are some observations (will improve this section over time):
+  - One key question was whether agent figures out planting bean at the right time - so far in any of reward combinations, I couldn't make the agent learn this trick!!! This will be the most interesting insight if proved properly : In RL setting, if there is a situation where for the same (state, action) combination, there is a certain time step in horizon that yields the best reward, can the existing algorithms learn such a phenomenon?
+    - One thing I should try is with sparse rewards (add cumulative growth internally in environment and return the reward only at end of episode, my current implementation uses dense reward model, each day the plant growth is returned as reward.)
+  - When corn without bean has a high reward than corn with bean, agent never plants bean.
---
+++ ToyProblems/problem1.py
@@ -0,0 +1,153 @@
+from gym import Env
+from gym.spaces import Discrete
+from gym.spaces import Tuple
+from gym.spaces import MultiBinary
+import numpy as np
+import random
+
+class SingleCellEnv(Env):
+    '''
+    Model a single cell where corn and bean can be planted.
+    If a bean is ever planted, it follows the rewards for corn from bean and bean from corn.
+    If bean is never planted, it follows the rewards from fertilizer (TODO)
+    taking the corn and growth and fert cost into account.
+    '''
+
+    def __init__(self,
+                harvest_period = 10,
+                fert_cost = 0.0,
+                corn_growth_no_support = 0.6,
+                corn_growth_with_fert = 1.0,
+                corn_growth_with_bean = 0.9,
+                bean_growth_with_support = 0.5,
+                bean_growth_no_support = 0.0):
+        self.harvest_period = harvest_period
+        self.fert_cost = fert_cost
+        self.corn_growth_no_support = corn_growth_no_support
+        self.corn_growth_with_fert = corn_growth_with_fert
+        self.corn_growth_with_bean = corn_growth_with_bean
+        self.bean_growth_with_support = bean_growth_with_support
+        self.bean_growth_no_support = bean_growth_no_support
+        self.state_space = Tuple((Discrete(2), Discrete(2)))
+        self.reset()
+        print('---------------------------------------------------------------------')
+        print('Environment Details:')
+        print('Reward Style:Dense') # TODO: Need to see what happens if we keep track of growth and return the reward at end of episode i.e, sparse reward.
+        print('Reward from Corn only: {}'.format(corn_growth_no_support))
+        print('Reward from Corn with bean: {}'.format(corn_growth_with_bean))
+        print('Reward from Bean when planted too late or too early: {}'.format(bean_growth_no_support))
+        print('Reward from Bean when planted after corn is grown enough: {}'.format(bean_growth_with_support))
+        print('---------------------------------------------------------------------')
+
+    def step(self, action):
+        done = (self.clock == self.harvest_period)
+        self.clock += 1
+
+        reward = 0.0
+        if self.state[0] == 1:
+            self.corn_age += 1
+            # TODO : Fertilizer?
+            reward += self.corn_growth_no_support if self.state[1] == 0 else self.corn_growth_with_bean
+        if self.state[1] == 1:
+            reward += self.bean_growth_with_support if self.bean_got_support else self.bean_growth_no_support
+
+        if action[0] == 1:
+            self.state = (1, self.state[1])
+
+        if action[1] == 1:
+            self.state = (self.state[0], 1)
+            if 4 <= self.corn_age <= 6:
+                self.bean_got_support = True
+        
+        return self.state, reward, done, None
+
+    def reset(self):
+        self.clock = 0
+        self.state = (0, 0)
+        self.corn_age = 0
+        self.bean_got_support = False
+        return self.state
+
+    def close(self):
+        pass
+
+    def render(self, mode):
+        print ('state: {}'.format(self.state))
+
+    def seed(self, seed=None):
+        return []
+
+    @property
+    def action_space(self):
+        first = Discrete(2 - self.state[0]) # For state value 0, we have 2 choices 0 and 1. For state value 1 we have only one choice.
+        second = Discrete(2 - self.state[1])
+        return Tuple((first, second))
+
+# Move this to a root location?
+class QLearningAgent:
+    '''A Q-learning agent that works with openAI gym interface based environment.'''
+    def __init__(self, env, learning_rate = 0.1, epsilon = 0.1, discount_rate = 0.9):
+        self.q_table = {}
+        self.env = env
+        self.alpha = learning_rate
+        self.gamma = discount_rate
+        self.epsilon = epsilon
+
+    def train(self, epochs):
+        print('Training for Epochs {}, using discount factor {}'.format(epochs, self.gamma))
+        for _ in range(epochs):
+            state = self.env.reset()
+            done = False
+            while not done:
+                if (random.uniform(0, 1) < self.epsilon) or (state not in self.q_table):
+                    action = self.env.action_space.sample()
+                else:
+                    action = max(self.q_table[state].keys(), key=lambda a:self.q_table[state][a])
+
+                next_state, reward, done, info = self.env.step(action)
+
+                next_state_value = 0.0 if next_state not in self.q_table else max(self.q_table[next_state].values())
+                if state not in self.q_table:
+                    self.q_table[state] = {}
+
+                self.q_table[state][action] = (1 - self.alpha) * self.q_table[state].get(action, 0.0) + self.alpha * (reward + self.gamma * next_state_value)
+                state = next_state
+
+    def test(self):
+        print('Test Run')
+        state = self.env.reset()
+        done = False
+        total_reward = 0.0
+        time_step = 0
+        while not done:
+            action = max(self.q_table[state].keys(), key=lambda a:self.q_table[state][a])
+            if action[0] == 1 and state[0] == 0:
+                print ('Corn planted at time step {}'.format(time_step))
+            if action[1] == 1 and state[1] == 0:
+                print ('Bean planted at time step {}'.format(time_step))
+            time_step += 1
+            next_state, reward, done, info = self.env.step(action)
+            total_reward += reward
+            state = next_state
+        print ('Total Reward = {}'.format(total_reward))
+
+    def train_and_test(self, epochs):
+        print('---------------------------------------------------------------------')
+        self.train(epochs)
+        self.test()
+        print('---------------------------------------------------------------------')
+
+def main():
+    env_specifications = [[0.5, 1.0, 0.7, 0.0],
+                          [0.5, 1.0, 1.0, 0.0],
+                          [2.0, 1.0, 0.5, 0.0],
+                          [2.0, 1.0, 1.0, 0.0],
+                          [0.5, 1.0, 1.5, 0.0]]
+    for spec in env_specifications:
+        env = SingleCellEnv(corn_growth_no_support=spec[0], corn_growth_with_bean=spec[1], bean_growth_with_support=spec[2], bean_growth_no_support=spec[3])
+        for gamma in [0.5, 0.9, 1.0]:
+            agent = QLearningAgent(env, discount_rate=gamma)
+            agent.train_and_test(1000)
+
+if __name__ == "__main__":
+    main()
